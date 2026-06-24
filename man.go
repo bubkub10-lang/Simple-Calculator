b762e881d3c452f34be17c5a187ba93a4521ab23
Sekiro <bubkub10@gmail.com>
2026-06-25 00:43:07 +0300

    added choose to action

--- man.go
+++ man.go
@@ -16,13 +16,36 @@ func askSecondNumber() float64 {
 	fmt.Scan(&secondNumber)
 	return secondNumber
 }
+func askWhatToDo() int {
+	var choose int
+	fmt.Println("What to Do?")
+	fmt.Printf("||[1]addition,")
+	fmt.Printf("[2]subtraction,")
+	fmt.Printf("[3]multiplication,")
+	fmt.Println("[4]division||")
+	fmt.Printf("--------")
+	fmt.Scan(&choose)
+	return choose
+}
 
-func main() {
+func whatToDo() {
+	thing := askWhatToDo()
 	firstNum := askNumber()
 	secondNum := askSecondNumber()
-	fmt.Println("")
-	fmt.Println("addition:", firstNum+secondNum)
-	fmt.Println("subtraction:", firstNum-secondNum)
-	fmt.Println("multiplication:", firstNum*secondNum)
-	fmt.Println("division:", firstNum/secondNum)
+	if thing == 1 {
+		fmt.Println("addittion", firstNum+secondNum)
+	}
+	if thing == 2 {
+		fmt.Println("subtraction", firstNum-secondNum)
+	}
+	if thing == 3 {
+		fmt.Println("multiplication", firstNum*secondNum)
+	}
+	if thing == 4 {
+		fmt.Println("division", firstNum/secondNum)
+	}
+}
+
+func main() {
+	whatToDo()
 }
